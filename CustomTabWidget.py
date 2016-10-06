f41a0c9bf4f13ddc3c7a358c3f2f3cf7e13e99d1
pragfang <pragfang@gmail.com>
2016-10-06 05:33:47 +0100

    refactoring

--- CustomTabWidget.py
+++ CustomTabWidget.py
@@ -1,8 +1,9 @@
-from PyQt4.QtGui import *
-
 from WebLayout import *
 
-class Trace_Tabs(QTabWidget):
+import sys
+
+
+class CustomTabWidget(QTabWidget):
 
     def __init__(self):
         QTabWidget.__init__(self)
@@ -21,10 +22,10 @@ class Trace_Tabs(QTabWidget):
         self.insertTab(1,QWidget(),'  +  ')
         self.tabBar().setTabButton(1, QTabBar.RightSide, None)
 
-        self.currentChanged.connect(self._add_trace)
-        self.tabCloseRequested.connect(self._close_tab)
+        self.currentChanged.connect(self._addTrace)
+        self.tabCloseRequested.connect(self._closeTab)
 
-    def _add_trace(self, index):
+    def _addTrace(self, index):
         '''last tab was clicked'''
         if index == self.count()-1 :
             '''add tab'''
@@ -35,7 +36,7 @@ class Trace_Tabs(QTabWidget):
             self.insertTab(index, newTabWidget, "New Tab")
             self.setCurrentIndex(index)
 
-    def _close_tab(self, index):
+    def _closeTab(self, index):
         if self.count() > 2:
             if index != 0:
                 if self.currentIndex() == index:
@@ -45,3 +46,5 @@ class Trace_Tabs(QTabWidget):
                 if self.currentIndex() == index:
                     self.setCurrentIndex(index+1)
                 self.removeTab(index)
+        else:
+            sys.exit()
--- Main.py
+++ Main.py
@@ -1,8 +1,3 @@
-from PyQt4.QtGui import *
-from PyQt4.QtWebKit import *
-from PyQt4.QtCore import *
-
-from WebLayout import *
 from CustomTabWidget import *
 
 import sys
@@ -12,15 +7,7 @@ class Main(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.tab1 = WebLayout()
-        self.tab1Widget = QWidget()
-        self.tab1Widget.setLayout(self.tab1)
-
-        self.tab2 = WebLayout()
-        self.tab2Widget = QWidget()
-        self.tab2Widget.setLayout(self.tab2)
-
-        self.tabs = Trace_Tabs()
+        self.tabs = CustomTabWidget()
 
         self.setWindowTitle("Web Browser")
         self.setCentralWidget(self.tabs)
